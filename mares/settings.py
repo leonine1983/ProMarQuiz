a9fd7cd9ad0965f6cc4e2e95017e5fcf54f195d8
Rogerio da Silva <rogerleonino@gmail.com>
2024-06-20 13:03:39 -0300

    feat: Configura arquivos estáticos, media, templates base e estilo CSS para formulários Win11
    Este commit adiciona a configuração de arquivos estáticos e media, criação de templates base para o projeto ProMar Quiz. Além disso, um estilo CSS exclusivo inspirado no Windows 11 foi desenvolvido para os formulários, proporcionando uma interface moderna. O CSS inclui layouts de grade e estilos de formulário para melhorar a experiência do usuário.
    
    Detalhes adicionais:
    - Arquivos estáticos (CSS, imagens) foram configurados e organizados adequadamente.
    - Templates base foram criados para manter consistência e reutilização de código.
    - Um estilo CSS foi implementado para refletir o design visual do Windows 11 nos formulários do aplicativo.

--- mares/settings.py
+++ mares/settings.py
@@ -44,7 +44,9 @@ ROOT_URLCONF = 'mares.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -93,9 +95,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -104,8 +106,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+    BASE_DIR / 'media'
+]
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
