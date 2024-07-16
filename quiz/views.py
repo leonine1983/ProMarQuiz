5095ebe0f35f1fae4dc3d61bc359ec36024cec9e
Rogerio da Silva <rogerleonino@gmail.com>
2024-07-16 15:05:52 -0300

    refator: Adicionado formatação nas tabelas

--- quiz/views.py
+++ quiz/views.py
@@ -162,6 +162,32 @@ def relatorio_completo(request):
                 perfil_visitante = PerfilVisitante.objects.all()  
                 mes_nome = calendar.month_name[datetime.datetime.now().month]
 
+            if mes_nome == 'January':
+                mes_nome = 'Janeiro'
+            elif mes_nome == 'February':
+                mes_nome = 'Fevereiro'
+            elif mes_nome == 'March':
+                mes_nome = 'Março'
+            elif mes_nome == 'April':
+                mes_nome = 'Abril'
+            elif mes_nome == 'May':
+                mes_nome = 'Maio'
+            elif mes_nome == 'June':
+                mes_nome='Junho'
+            elif mes_nome == 'July':
+                mes_nome='Julho'
+            elif mes_nome == 'August':
+                mes_nome='Agosto'
+            elif mes_nome == 'September':
+                mes_nome='Setembro'
+            elif mes_nome == 'October':
+                mes_nome='Outubro'
+            elif mes_nome == 'November':
+                mes_nome='Novembro'
+            else:
+                mes_nome='Dezembro'
+
+
             # Quantidade de visitantes
             total_visitantes = perfil_visitante.count()
 
@@ -199,73 +225,112 @@ def relatorio_completo(request):
             return render(request, 'relatorio_completo.html', context)
         
 
-def exportar_para_excel(context):   
+import xlsxwriter
+
+def exportar_para_excel(context):
+    # Inicializa o workbook e worksheet
     workbook = xlsxwriter.Workbook('relatorio_promar.xlsx')
     worksheet = workbook.add_worksheet()
 
+    # Definindo formatos
     bold = workbook.add_format({'bold': True})
+    normal = workbook.add_format()
+
+    # Ajustes de estilo para se assemelhar ao Windows 11/12
+    worksheet.set_default_row(20)  # Altura padrão das linhas
+    worksheet.set_column('A:B', 20)  # Largura padrão das colunas
+
+    # Formato para cabeçalhos
+    header_format = workbook.add_format({
+        'bold': True,
+        'align': 'center',
+        'valign': 'vcenter',
+        'fg_color': '#F2F2F2',  # Cor de fundo cinza claro
+        'border': 1  # Borda fina ao redor
+    })
 
     # Escrever cabeçalhos
     headers = ['Categoria', 'Valor']
     for col, header in enumerate(headers):
-        worksheet.write(0, col, header, bold)
+        worksheet.write(0, col, header, header_format)
 
     # Escrever dados usando o contexto recebido
     row = 1
-    worksheet.write(row, 0, "Mês")
-    worksheet.write(row, 1, context['mes_nome'])
-    row +=1    
-    worksheet.write(row, 0, 'Total de Visitantes')
-    worksheet.write(row, 1, context['total_visitantes'])
+    col=2
+    data_format = workbook.add_format({
+        'align': 'left',
+        'valign': 'vcenter',
+        'border': 1  # Borda fina ao redor
+    })
+
+    # Dados individuais
+    worksheet.write(row, 0, "Mês", data_format)
+    worksheet.write(row, 1, context['mes_nome'], data_format)
+    row += 1    
+    worksheet.write(row, 0, 'Total de Visitantes', data_format)
+    worksheet.write(row, 1, context['total_visitantes'], data_format)
     row += 1
-    worksheet.write(row, 0, 'Total de Acertos')
-    worksheet.write(row, 1, context['total_acertos'])
+    worksheet.write(row, 0, 'Total de Acertos', data_format)
+    worksheet.write(row, 1, context['total_acertos'], data_format)
     row += 1
-    worksheet.write(row, 0, 'Total de Erros')
-    worksheet.write(row, 1, context['total_erros'])
+    worksheet.write(row, 0, 'Total de Erros', data_format)
+    worksheet.write(row, 1, context['total_erros'], data_format)
     row += 2
 
-    worksheet.write(row, 0, 'Respostas Mais Acertadas', bold)
+    # Títulos secundários
+    secondary_title_format = workbook.add_format({
+        'bold': True,
+        'align': 'left',
+        'valign': 'vcenter',
+        'fg_color': '#D9EAD3',  # Cor de fundo verde claro
+        'border': 1  # Borda fina ao redor
+    })
+
+    # Respostas mais acertadas
+    worksheet.write(row, 0, 'Respostas Mais Acertadas', secondary_title_format)
     row += 1
-    worksheet.write(row, 0, 'Resposta')
-    worksheet.write(row, 1, 'Total')
+    worksheet.write(row, 0, 'Resposta', header_format)
+    worksheet.write(row, 1, 'Total', header_format)
     row += 1
     for resposta in context['respostas_acertadas']:
-        worksheet.write(row, 0, resposta['resposta__texto_resposta'])
-        worksheet.write(row, 1, resposta['total'])
+        worksheet.write(row, 0, resposta['resposta__texto_resposta'], data_format)
+        worksheet.write(row, 1, resposta['total'], data_format)
         row += 1
     row += 1
 
-    worksheet.write(row, 0, 'Cidades com Melhor Desempenho', bold)
+    # Cidades com melhor desempenho
+    worksheet.write(row, 0, 'Cidades com Melhor Desempenho', secondary_title_format)
     row += 1
-    worksheet.write(row, 0, 'Cidade')
-    worksheet.write(row, 1, 'Total de Acertos')
+    worksheet.write(row, 0, 'Cidade', header_format)
+    worksheet.write(row, 1, 'Total de Acertos', header_format)
     row += 1
     for cidade in context['cidades_acertos']:
-        worksheet.write(row, 0, cidade['municipio_escola'])
-        worksheet.write(row, 1, cidade['total_acertos'])
+        worksheet.write(row, 0, cidade['municipio_escola'], data_format)
+        worksheet.write(row, 1, cidade['total_acertos'], data_format)
         row += 1
     row += 1
 
-    worksheet.write(row, 0, 'Notas Mais Dadas', bold)
+    # Notas mais dadas
+    worksheet.write(row, 0, 'Notas Mais Dadas', secondary_title_format)
     row += 1
-    worksheet.write(row, 0, 'Nota')
-    worksheet.write(row, 1, 'Total')
+    worksheet.write(row, 0, 'Nota', header_format)
+    worksheet.write(row, 1, 'Total', header_format)
     row += 1
     for nota in context['notas_mais_dadas']:
-        worksheet.write(row, 0, nota['nota_visita'])
-        worksheet.write(row, 1, nota['total'])
+        worksheet.write(row, 0, nota['nota_visita'], data_format)
+        worksheet.write(row, 1, nota['total'], data_format)
         row += 1
     row += 1
 
-    worksheet.write(row, 0, 'Idades Mais Visitadas', bold)
+    # Idades mais visitadas
+    worksheet.write(row, 0, 'Idades Mais Visitadas', secondary_title_format)
     row += 1
-    worksheet.write(row, 0, 'Idade')
-    worksheet.write(row, 1, 'Total')
+    worksheet.write(row, 0, 'Idade', header_format)
+    worksheet.write(row, 1, 'Total', header_format)
     row += 1
     for idade in context['idades_mais_visitadas']:
-        worksheet.write(row, 0, idade['idade'])
-        worksheet.write(row, 1, idade['total'])
+        worksheet.write(row, 0, idade['idade'], data_format)
+        worksheet.write(row, 1, idade['total'], data_format)
         row += 1
 
     workbook.close()
@@ -276,4 +341,3 @@ def exportar_para_excel(context):
         response['Content-Disposition'] = 'attachment; filename=relatorio_promar.xlsx'
 
     return response
-
